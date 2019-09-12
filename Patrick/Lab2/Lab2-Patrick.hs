f3bfb0cde2d144884fc422f55bf36a89a6c413cf
Patrick Fennema <patrick@bryce.demon.nl>
2019-09-12 16:34:56 +0200

    Added ROT13 assignment start

--- Patrick/Lab2/Lab2-Patrick.hs
+++ Patrick/Lab2/Lab2-Patrick.hs
@@ -3,12 +3,14 @@ import Test.QuickCheck
 import Data.List
 import Data.Char
 import Data.Ord
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 -- Redefinition of forall, Lab2.hs is wrong
 forall :: [a] -> (a -> Bool) -> Bool
 forall = flip all
 
--- Excercise 1: random in open interval
+-- Excercise 1: random in open interval, time spent: 1 hour
 -- Usage: countQuartiles <number of samples
 -- Returns an array with 4 items, for the 4 different quartiles
 getQuartile :: [Float] -> Float -> Float -> Int
@@ -20,7 +22,7 @@ countQuartiles n = do
       (getQuartile y 0.5 0.75):
       (getQuartile y 0.75 1.0):[])
 
--- Excercise 2: recognizing Triangles, time spent: 1 hour
+-- Excercise 2: recognizing Triangles, time spent: 1.5 hour
 {-
 findTriangle :: Integer -> Integer -> Integer -> Shape
 findTriangle a b c =
@@ -72,7 +74,7 @@ testTriangle=
  - which testcase failed (not just True/False output)
  -}
 
--- Excercise 3: Testing properties strength
+-- Excercise 3: Testing properties strength, time spent: 30 mins
 stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
 stronger xs p q = forall xs (\ x -> p x --> q x)
 weaker   xs p q = stronger xs q p 
@@ -89,7 +91,7 @@ prop3And4
 prop2
 WEAKEST-}
 
--- Excercise 4: Recognizing Permutations
+-- Excercise 4: Recognizing Permutations, time spent: 30 mins
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation a b = null (a \\ b) && null (b \\ a)
 -- explanation: use list difference operator to find items only occuring in a or only in b.
@@ -102,4 +104,45 @@ isPermutation a b = null (a \\ b) && null (b \\ a)
 
 testIsPermutation =
     (isPermutation [1,2,3] [2,1,3]) &&
-    not (isPermutation [1,2,3] [2,4,3])
+    not (isPermutation [1,2,3] [2,4,3]) &&
+    (isPermutation [1.5,2.4,3.3] [2.4,1.5,3.3]) &&
+    not (isPermutation [1.5,2.4,3.3] [2.4,1.3,3.5])
+
+-- Exercise 5: Recognizing and generating derangements
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement a b = isPermutation a b && all (==True) [x/=y | (x,y)<-(zip a b)]
+
+deran :: (Eq a, Num a, Enum a) => a -> [[a]]
+deran 0 = []
+deran 1 = []
+deran n = let source = [0..(n-1)]
+              mutations = permutations source in
+    [x | x<-mutations, isDerangement source x]
+
+-- properties isDerangement:
+-- 1. list must be equal length
+-- 2. lists must be permutations of eachother
+-- The properties can be ranked as follows:
+--
+listLength x y = length x == length y
+curryPermutation n = isPermutation [1,2,3,4] n
+curryLength n = listLength [1,2,3,4] n
+-- stronger [[2,3,4,1], [5,2,1,3]] curryPermutation curryLength
+--
+-- The result here is (in order from strongest to weakest):
+-- permutation
+-- list length
+
+-- Exercise 6: Implementing and testing ROT13 encoding
+-- Specification
+-- ROT13 maps an input character in [A..Z]++[a..z] to 
+-- an output character in [N..Z]++[A..M]++[n..z]++[a..m] (the index stays the same)
+rot13' s | (c >= 65) && (c <= 77) = chr (c+13)
+        | (c >= 78) && (c <= 90) = chr (c-13)
+        | (c >= 97) && (c <= 109) = chr (c+13)
+        | (c >= 110) && (c <= 122) = chr (c-13)
+        | otherwise = '?'
+        where c = ord s
+rot13 s = [rot13' c | c <- s]
+
+-- TODO
