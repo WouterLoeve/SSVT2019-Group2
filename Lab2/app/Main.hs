8bb9b0370948f114316a877e3a8ea18400923420
Mike Mourcous <m.mourcous@hotmail.com>
2019-09-17 22:07:02 +0200

    triangle comments, bool to property en main

--- Lab2/app/Main.hs
+++ Lab2/app/Main.hs
@@ -5,4 +5,25 @@ import Test.QuickCheck
 import Data.Char
 
 main = do
-    printf ""
+    -- printf "== Assignment 1 ==\n"
+    -- printf "%s\n" quartileTest
+    -- printf "== Assignment 2 ==\n"
+    -- printf "%s\n" testTriangle
+    -- printf "== Assignment 3 ==\n"
+
+    printf "== Assignment 4 ==\n"
+    quickCheck prop_sameListLengthPerm
+    -- quickCheck prop_numOccurencePerm
+    --
+    -- printf "== Assignment 5 ==\n"
+    -- quickCheck prop_sameObjectsDeran
+    -- quickCheck prop_sameListLengthDeran
+    -- quickCheck prop_numOccurenceDeran
+    -- quickCheck prop_samePosDeran
+    -- quickCheck prop_unequalDeran
+    --
+    -- printf "== Assignment 6 ==\n"
+    -- testRot13Props
+    --
+    -- printf "== Assignment 7 ==\n"
+    -- printf "%s\n" ibanCorrectTests
