c3d4c182153af3b923fa4ac587fe8af26328d4c1
Hendrik Huang <hendrik.huang@student.uva.nl>
2019-09-27 00:09:14 +0200

    Add implementations for 1-5 (check out dat closure)

--- Hendrik/Lab4/src/Lab4solutions.hs
+++ Hendrik/Lab4/src/Lab4solutions.hs
@@ -1,4 +1,4 @@
-module Lab3solutions where
+module Lab4solutions where
 import Data.List
 import Data.Char
 import Data.Tuple
@@ -6,6 +6,9 @@ import Test.QuickCheck
 import Control.Monad
 import SetOrd
 import Lecture3
+
+import System.Random
+import Data.Function
 
 {-
  - testRunhelper helps print results
@@ -20,4 +23,50 @@ testRunHelper testName numCases numPass = do
  - Exercise 1
  - Time: min
 -}
+genSetInt :: Int -> (Int, Int) -> IO (Set Int)
+genSetInt lengthR numR = do
+    n <- getStdRandom (randomR (0, lengthR))
+    gen <- getStdGen
+    return . list2set . take n $ randomRs numR gen
+
+genSetInt' :: Int -> (Int, Int) -> Gen (Set Int)
+genSetInt' lengthR numR = Set <$> resize lengthR (listOf arbitrary)
+
+{- E2
+-}
+set2list :: Set a -> [a]
+set2list (Set l) = l
+
+(>*<) :: Ord a => Set a -> Set a -> Set a
+a >*< b = list2set $ set2list a `intersect` set2list b
+
+(>+<) :: Ord a => Set a -> Set a -> Set a
+a >+< b = list2set $ set2list a ++ set2list b
+
+(>\\<) :: Ord a => Set a -> Set a -> Set a
+a >\\< b = list2set $ set2list a \\ set2list b
+
+{- E3
+-}
+
+type Rel a = [(a,a)]
+
+symClos :: Ord a => Rel a -> Rel a
+symClos r = union r $ swap <$> r 
+
+{- E4
+-}
+
+isSerial :: Eq a => [a] -> Rel a -> Bool
+isSerial d r = all (`elem` r') d
+    where r' = fst <$> filter (\ (_, b) -> b `elem` d) r
+
+{- E5
+-}
+infixr 5 @@
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+trClos :: Ord a => Rel a -> Rel a 
+trClos r = fix (\ f s -> if s == s `union` (r @@ s) then s else f $ s `union` (r @@ s)) r
 
