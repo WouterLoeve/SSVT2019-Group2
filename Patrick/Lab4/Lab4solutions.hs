c39630f1ad49fb5b5ce6470789cd9161a03a28fb
Patrick Fennema <patrick@bryce.demon.nl>
2019-09-27 11:25:10 +0200

    Done until assignment 5

--- Patrick/Lab4/Lab4solutions.hs
+++ Patrick/Lab4/Lab4solutions.hs
@@ -4,6 +4,7 @@ import Data.Char
 import Data.Tuple
 import Test.QuickCheck
 import Control.Monad
+import System.Random
 import SetOrd
 import Lecture3
 
@@ -16,13 +17,17 @@ import Lecture3
 --tmp_prop :: set char -> bool
 --tmp_prop s = true
 
-arbSet :: Arbitrary a => Int -> Gen (SetOrd.Set a)
+--arbSetInt :: Int -> IO (Set Int)
+--arbSetInt s = do
+--    return sample $ resize 1000 (arbitrary :: IO (Set Int))
+
+arbSet :: Arbitrary a => Int -> Gen (Set a)
 arbSet s = do
     n <- choose (0, s)
     l <- vectorOf n arbitrary
     return $ Set l
 
-instance Arbitrary a => Arbitrary (SetOrd.Set a) where
+instance Arbitrary a => Arbitrary (Set a) where
     arbitrary = sized arbSet
 
 {-
@@ -47,7 +52,7 @@ symClos r = union r [(b,a) | (a,b) <- r]
 
 {-
  - Exercise 4
- - Time:  min
+ - Time: 10 min
 -}
 
 -- Serial means that the domain of input is a subset of the range of the relation
@@ -55,4 +60,16 @@ isSerial :: Eq a => [a] -> Rel a -> Bool
 isSerial l r = all (==True) [x `elem` relRange | x <- l]
     where relRange = (map fst r)
 
+{-
+ - Exercise 5
+ - Time: 20 min
+ - The trClos function recursively calculates the union of r and r @@ r until this
+ - operation does not yield a different result than the input r anymore
+-}
+infixr 5 @@
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
+trClos :: Ord a => Rel a -> Rel a 
+trClos r = if r == r1 then r1 else trClos r1
+    where r1 = union r (r @@ r)
